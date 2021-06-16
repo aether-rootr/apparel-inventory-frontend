d64cbf7cd07786083beb1438d26a2548478ddadb
aether-rootr <aetherrootr@outlook.com>
2021-06-16 14:07:39 +0000

    add header

---
+++ src/components/header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+  <NPageHeader >
+    <template #title>
+      <NA style="text-decoration: none; color: inherit;">
+        服装库存管理系统
+      </NA>
+    </template>
+    <template #header></template>
+    <template #avatar>
+      <NButton text style="font-size: 24px;" @click="handleClick">
+        <NIcon>
+          <AlignJustified />
+        </NIcon>
+      </NButton>
+    </template>
+    <template #footer><NDivider/></template>
+  </NPageHeader>
+  </div>
+</template>
+
+<script>
+import {
+  NPageHeader,
+  NDivider,
+  NA,
+  NIcon,
+  NButton,
+} from 'naive-ui'
+
+import {AlignJustified} from '@vicons/tabler'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    NPageHeader,
+    NDivider,
+    NA,
+    NIcon,
+    NButton,
+    AlignJustified,
+  },
+  setup() {
+    const 
+  }
+})
+</script>
---
+++ src/router/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    
+  ]
+});
+
+// // 导航守卫
+// // 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
+// router.beforeEach((to, from, next) => {
+//   if (to.path === '/login') {
+//     next();
+//   } else {
+//     let token = localStorage.getItem('Authorization');
+ 
+//     if (token === null || token === '') {
+//       next('/login');
+//     } else {
+//       next();
+//     }
+//   }
+// });
+ 
+export default router;
